1defd4c61a172de630b463cea46ecee4b09d9dcc
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-17 16:32:37 -0300

    Agregamos tres paquetes: presentacion/repository/router

--- app/src/main/java/com/example/appweather/Clima.kt
+++ app/src/main/java/com/example/appweather/Clima.kt
@@ -1,1 +1,12 @@
 package com.example.appweather
+
+data class Clima(
+    val temperatura: Int,
+    val humedad: Float,
+    val ciudad: String,
+    val st: Int,
+    val viento: Int,
+    val latitud: Long,
+    val longitud: Long,
+    val estado: String
+)
