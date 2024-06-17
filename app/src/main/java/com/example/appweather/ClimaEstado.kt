1e2db62bf7bd02b413bcdae87fa2b6d139454f85
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-17 15:47:37 -0300

    Actualización ClimaEstado y clase sellada

--- app/src/main/java/com/example/appweather/ClimaEstado.kt
+++ app/src/main/java/com/example/appweather/ClimaEstado.kt
@@ -1,16 +1,18 @@
 package com.example.appweather
 
 sealed class ClimaEstado {
-        data class Exitoso (
-            val ciudad: String = "",
-            val temperatura: Double = 0.0,
-            val descripcion: String= "",
-            val st :Double = 0.0,
-        ) : ClimaEstado ()
+    data class Exitoso(
+        val ciudad: String = "",
+        val temperatura: Double = 0.0,
+        val descripcion: String = "",
+        val st: Double = 0.0,
+    ) : ClimaEstado()
 
+    data class Error(
+        val mensaje: String = "",
+    ) : ClimaEstado()
 
-
-
-
+    data object Vacio : ClimaEstado()
+    data object Cargando : ClimaEstado()
 
 }
