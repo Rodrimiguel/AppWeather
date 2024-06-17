1e5228cc5383114c5df1b224cec0e55979c3653a
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-17 19:48:47 -0300

    Creacion_Vistas/ClimaView&Funciones

---
+++ app/src/main/java/com/example/appweather/presentacion/ClimaPage.kt
@@ -0,0 +1,12 @@
+package com.example.appweather.presentacion
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun MainPage () {
+        val viewModel :
+
+
+
+}
---
+++ app/src/main/java/com/example/appweather/presentacion/ClimaView.kt
@@ -0,0 +1,4 @@
+package com.example.appweather.presentacion
+
+class ClimaView {
+}
