92f1af405160ed5c107414e8a9e1fa7003cabb51
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-19 19:38:19 -0300

    Corrigiendo algunos errores en RepositorioApi

--- app/src/main/java/com/example/appweather/repository/RepositorioApi.kt
+++ app/src/main/java/com/example/appweather/repository/RepositorioApi.kt
@@ -2,7 +2,6 @@ package com.example.appweather.repository
 
 import com.example.AppWeather.repository.modelos.Ciudad
 import com.example.AppWeather.repository.modelos.Clima
-import com.example.AppWeather.repository.modelos.Clima2
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -14,51 +13,43 @@ import kotlinx.serialization.json.Json
 
 class RepositorioApi : Repositorio {
 
-    private val apiKey = "dfa1e508561765575ee4f6856ef4b3ea"
+        private val apikey = "dfa1e508561765575ee4f6856ef4b3ea"
 
-    private val cliente = HttpClient() {
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-            })
+        private val cliente = HttpClient(){
+            install(ContentNegotiation){
+                json(Json { ignoreUnknownKeys = true })
+            }
         }
-    }
 
     override suspend fun buscarCiudad(ciudad: String): List<Ciudad> {
         val respuesta = cliente.get() {
-            parameter("lat", 44.34)
-            parameter("lon", 10.99)
-            parameter("appid", apiKey)
+            parameter("q", ciudad)
+            parameter("limit", 100)
+            parameter("appid", apikey)
         }
         if (respuesta.status == HttpStatusCode.OK) {
             val ciudades = respuesta.body<List<Ciudad>>()
             return ciudades
-        } else {
-            throw Exception()
+        }else{
+            throw Exception ()
         }
     }
+
     override suspend fun traerClima(ciudad: Ciudad): Clima {
         val respuesta = cliente.get(){
-            parameter("lat", 0.0)
-            parameter("lon", 0.0)
-            parameter("units","metric")
-            parameter("appid",apiKey)
+            parameter("base", 00.00)
+            parameter("lon", 00.00)
+            parameter("units", "metric")
+            parameter("appid", apikey)
         }
         if (respuesta.status == HttpStatusCode.OK){
             val clima = respuesta.body<Clima>()
             return clima
-        }else{
+        }else {
             throw Exception()
         }
     }
-
-    override suspend fun traerPronostico(lat: Float, lon: Float): List<Clima2> {
+    override suspend fun traerPronostico(Ciudad: Ciudad): List<Clima> {
         TODO("Not yet implemented")
     }
-
 }
-
-
-
-
-
--- app/src/main/java/com/example/appweather/repository/modelos/Clima.kt
+++ app/src/main/java/com/example/appweather/repository/modelos/Clima.kt
@@ -1,58 +1,48 @@
 package com.example.AppWeather.repository.modelos
 
+import kotlinx.serialization.Serializable
 
 
-class Clima(
-    val temperatura: Int,
-    val humedad: Float,
-    val ciudad: String,
-    val st: Int,
-    val viento: Int,
-    val latitud: Long,
-    val longitud: Long,
-    val estado: String
-)
-
-class Clima2(
+@Serializable
+data class Clima(
     val base: String,
     val name: String,
+    val coord: Coord,
+    val weather: List<Weather>,
+    val main: Main,
+    val wind: Wind,
+    val clouds: Clouds,
 )
 
-class Coord(
+@Serializable
+data class Coord(
     val lon: Double,
     val lat: Double,
 )
-
-class Weather(
+@Serializable
+data class Weather(
     val id: Long,
     val main: String,
     val description: String,
     val icon: String,
 )
-
-class Main(
+@Serializable
+data class Main(
     val temp: Double,
-    val feelsLike: Double,
-    val tempMin: Double,
-    val tempMax: Double,
+    val feels_like: Double,
+    val temp_min: Double,
+    val temp_max: Double,
     val pressure: Long,
     val humidity: Long,
 )
-
-class Wind(
+@Serializable
+data class Wind(
     val speed: Double,
     val deg: Long,
-    val gust: Double,
+    val gust: Double? = null,
 )
 
-class Clouds(
+@Serializable
+data class Clouds(
     val all: Long,
-)
-
-class Sys(
-    val type: Long,
-    val id: Long,
-    val country: String,
-    val sunrise: Long,
-    val sunset: Long,
 )
