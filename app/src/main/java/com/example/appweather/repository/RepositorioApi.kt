a1841ef0078521f9f44f4f2cef4710b8175250ab
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-19 18:59:24 -0300

    Reestructuracion de Repository/RepositorioApi con algunos problemas/detalles a corregir

--- app/src/main/java/com/example/appweather/repository/RepositorioApi.kt
+++ app/src/main/java/com/example/appweather/repository/RepositorioApi.kt
@@ -1,18 +1,23 @@
 package com.example.appweather.repository
 
 import com.example.AppWeather.repository.modelos.Ciudad
+import com.example.AppWeather.repository.modelos.Clima
 import com.example.AppWeather.repository.modelos.Clima2
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-class RepositorioApi : Repositorio{
+class RepositorioApi : Repositorio {
 
     private val apiKey = "dfa1e508561765575ee4f6856ef4b3ea"
 
-    private val cliente = HttpClient(){
-        install(ContentNegotiation){
+    private val cliente = HttpClient() {
+        install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
             })
@@ -20,14 +25,34 @@ class RepositorioApi : Repositorio{
     }
 
     override suspend fun buscarCiudad(ciudad: String): List<Ciudad> {
-        TODO("Not yet implemented")
+        val respuesta = cliente.get() {
+            parameter("lat", 44.34)
+            parameter("lon", 10.99)
+            parameter("appid", apiKey)
+        }
+        if (respuesta.status == HttpStatusCode.OK) {
+            val ciudades = respuesta.body<List<Ciudad>>()
+            return ciudades
+        } else {
+            throw Exception()
+        }
+    }
+    override suspend fun traerClima(ciudad: Ciudad): Clima {
+        val respuesta = cliente.get(){
+            parameter("lat", 0.0)
+            parameter("lon", 0.0)
+            parameter("units","metric")
+            parameter("appid",apiKey)
+        }
+        if (respuesta.status == HttpStatusCode.OK){
+            val clima = respuesta.body<Clima>()
+            return clima
+        }else{
+            throw Exception()
+        }
     }
 
-    override suspend fun traerClima(ciudad: Ciudad): Clima2 {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun traerPronostico(Ciudad: Ciudad): List<Clima2> {
+    override suspend fun traerPronostico(lat: Float, lon: Float): List<Clima2> {
         TODO("Not yet implemented")
     }
 
