7fb2fb137e465d474efd1a713fa2dfa26a328354
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-20 19:38:12 -0300

    Optimizacion/Reparando Errores codigo RepositorioApi

--- app/src/main/java/com/example/appweather/repository/RepositorioApi.kt
+++ app/src/main/java/com/example/appweather/repository/RepositorioApi.kt
@@ -2,8 +2,8 @@ package com.example.appweather.repository
 
 import com.example.AppWeather.repository.modelos.Ciudad
 import com.example.AppWeather.repository.modelos.Clima
+import com.example.appweather.repository.modelos.ForecastDTO
 import com.example.appweather.repository.modelos.ListForecast
-//import com.example.AppWeather.repository.modelos.ForecastDTO
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -15,7 +15,7 @@ import kotlinx.serialization.json.Json
 
 class RepositorioApi : Repositorio {
 
-    private val apikey = ""
+    private val apikey = "95e93e4f7a36fc511148468d1774792d"
 
     private val cliente = HttpClient() {
         install(ContentNegotiation) {
@@ -40,7 +40,7 @@ class RepositorioApi : Repositorio {
     }
 
     override suspend fun traerClima(lat: Float, lon: Float): Clima {
-        val respuesta = cliente.get() {
+        val respuesta = cliente.get("https://api.openweathermap.org/geo/1.0/direct") {
             parameter("base", 00.00)
             parameter("lon", 00.00)
             parameter("units", "metric")
@@ -55,13 +55,15 @@ class RepositorioApi : Repositorio {
     }
 
     override suspend fun traerPronostico(nombre: String): List<ListForecast> {
-        val respuesta = cliente.get("") {
+        val respuesta = cliente.get("https://api.openweathermap.org/data/2.5/weather") {
+            // ?q=paris&appid=8e0af9a65e0f0b0663509216ea77440d
             parameter("q", nombre)
-            parameter("units", "metric")
-            parameter("appid", apiKey)
+            parameter("units","metric")
+            parameter("appid",apikey)
+
         }
         if (respuesta.status == HttpStatusCode.OK) {
-            val forecast = respuesta.body<>()
+            val forecast = respuesta.body<ForecastDTO>()
             return forecast.list
         } else {
             throw Exception()
